154ea8eeaaf47f3b7c6a053dee4cf83ddf87e8e7
MaHappyCode <90353237+MaHappyCode@users.noreply.github.com>
2021-11-24 21:13:19 +0100

    New stuff
    the good stuff

--- main.js
+++ main.js
@@ -1,46 +1,17 @@
-function startTime() {
+'use strict';
 
-    const today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-
-        m = checkTime(m);
-        s = checkTime(s);
-
-        function checkTime(i) {
-
-            if ( i < 10 ) { i = "0" + i};
-            return i;
-         } if ( h > 12 ) {
-             h = h - 00;
-             if ( h == 12) {
-                 h = checkTime(h);
-            document.getElementById ("time").innerHTML = h + ":" 
-            + m + ":" + s + " AM";
-        }else{
-            h = checkTime(h);
-            document.getElementById("time").innerHTML = h + ":"
-            + m + ":" + s + " PM"
-        }
-}else {
-    document.getElementById("time").innerHTML = h + ":" + 
-    m + ":" + s + " AM";
-    }
-    setTimeout(startTime,1000);
+function load () {
+  console.log('App is now loading.');
+  return exp.getCurrentDevice().then(function (device) {
+    document.getElementById('name').textContent = device.name;
+  });
 }
-    
-    function myFunction() {
-        console.log(myFunction);
-        var x = document.getElementById("hide");
-        if (x.style.display === "none") {
-          x.style.display = "block";
-        } else {
-          x.style.display = "none";
-        }
-      }
-    
+
+function play () {
+  console.log('App is now playing.');
+}
 
 
-
-    
+function unload () {
+  // clean up any mess we made
+}
